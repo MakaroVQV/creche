5a8cafcc178198dd3a26263279b4541488eea5f6
Erick D <eridanpersan@gmail.com>
2023-04-24 20:15:26 -0300

    att

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Creche</b>LTE',
+    'logo' => '<b>Jardim dos Sonhos</b>',
     'logo_img' => 'vendor/adminlte/dist/img/logo-Creche-escola-Jardim-dos-sonhos.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
