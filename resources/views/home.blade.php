16bca1f06dc9ae1554e3807bca64f59e6decceb0
Erick D <eridanpersan@gmail.com>
2023-04-26 21:47:55 -0300

    att

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('/css/menuStyle.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
 
     <title>Creche</title>
 </head>
