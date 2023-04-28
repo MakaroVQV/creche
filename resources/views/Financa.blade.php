30bc5cac2af380c3f8e7bfa185d81f70f27df26b
Luis Felipe <107706177+felipanos@users.noreply.github.com>
2023-04-28 20:19:50 -0300

    aat

--- resources/views/Financa.blade.php
+++ resources/views/Financa.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{asset('/css/medico.css')}}">
+  <link rel="stylesheet" href="{{asset('/css/financa.css')}}">
   <title>Creche</title>
 </head>
 
@@ -27,18 +27,27 @@
 
     <!--Menu lateral-->
     <div id="mySidenav" class="sidenav">
-  <div class="side">
-    <a href="javascript:void(0)" onclick="closeNav()"><img src="{{asset('/imagens/icon menu.png')}}" class="menu-cinza"></a>
-    <div><img src="{{asset('/imagens/creche.png')}}" class="logo-sidebar"></div>
-  </div>
-  <div class="iten-menu">
-    <a href="#"><img src="{{asset('/imagens/icon calendario cinza.png')}}"><p>Calendario</p></a>
-    <a href="financa.html"><img src="{{asset('/imagens/icon financeiro cinza.png')}}"><p>Financeiro</p></a>
-    <a href="menuPrincipal.html"><img src="{{asset('/imagens/icon home azul.png')}}"><p>Home</p></a>
-    <a href="#"><img src="{{asset('/imagens/icon saude cinza.png')}}"><p>Saude</p></a>
-    <a href="#"><img src="{{asset('/imagens/icon user cinza.png')}}"><p>Perfil</p></a>
-  </div>
-
+      <div class="side">
+        <a href="javascript:void(0)" onclick="closeNav()"><img src="{{asset('/imagens/icon menu.png')}}" class="menu-cinza"></a>
+        <div><img src="{{asset('/imagens/creche.png')}}" class="logo-sidebar"></div>
+      </div>
+      <div class="iten-menu">
+        <a href="{{ url('/calendar')}}"><img src="{{asset('/imagens/icon calendario cinza.png')}}">
+          <p>Calendario</p>
+        </a>
+        <a href="{{ url('/financa')}}"><img src="{{asset('/imagens/icon financeiro cinza.png')}}">
+          <p>Financeiro</p>
+        </a>
+        <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon home azul.png')}}">
+          <p>Home</p>
+        </a>
+        <a href="{{ url('/medico')}}"><img src="{{asset('/imagens/icon saude cinza.png')}}">
+          <p>Saude</p>
+        </a>
+        <a href="{{ url('/perfil')}}"><img src="{{asset('/imagens/icon user cinza.png')}}">
+          <p>Perfil</p>
+        </a>
+      </div>
       <!--Footer do menu lateral-->
       <div class="footer-menu">
         <a href="index.html"><img src="{{asset('/imagens/sair.png')}}" class="sair">
@@ -47,68 +56,58 @@
       </div>
 
       <div class="copyriht">
-        <img src="{{asset('/imagens/icon copyright preto.png')}}" class="copyright">
+        <img src="{{asset('/imagens/icon_copyright.png')}}" class="copyright">
         <p><strong>2023 FROM NEWPATH</strong></p>
       </div>
     </div>
 
-  </div>
+    <!--Financeiro: Pendentes, Pagas e Dados-->
 
+    <div class="financa">
 
-  <!--Ficha, Atestado e cardápio-->
+      <a href="#pendente">
+        <p>Pendentes</p>
 
-  <div class="invisible">
-    <div class="cards">
-      <img src="{{asset('/imagens/icon ficha medica.png')}}">
-      <p>Ficha Médica</p>
+      </a>
+
+      <a href="#pagas">
+        <p>Pagas</p>
+
+      </a>
+
+      <a href="#dados">
+        <p>Dados</p>
+      </a>
+
     </div>
 
-    <div class="cards">
-      <img src="{{asset('/imagens/icon atestados.png')}}">
-      <p>Atestado</p>
+    <div id="conteudo">
+
+
+
+
+
+    
     </div>
 
-    <div class="cards">
-      <img src="{{asset('/imagens/icon cardapio.png')}}">
-      <p>Cardápio</p>
-    </div>
-  </div>
 
 
+    <script>
+      /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
+      }
 
-  <!--Menu lateral-->
-  <div id="mySidenav" class="sidenav">
-    <div class="side">
-      <a href="javascript:void(0)" onclick="closeNav()"><img src="{{asset('/imagens/icon menu.png')}}" class="menu-cinza"></a>
-      <div><img src="{{asset('/imagens/creche.png')}}" class="logo-sidebar"></div>
-    </div>
-    <a href="javascript:void(0)" onclick="closeNav()"><img src="{{ url('/imagens/seta-azul.png')}}" class="seta-azul"></a>
-    <div class="iten-menu">
-      <a href="{{ url('/notificado')}}"><img src="{{asset('/imagens/icon calendario cinza.png')}}">Calendario</a>
-      <a href="{{ url('/financa')}}"><img src="{{asset('/imagens/icon financeiro cinza.png')}}">Financeiro</a>
-      <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon home azul.png')}}">Home</a>
-      <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon saude cinza.png')}}">Saude</a>
-      <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon menu.png')}}">Perfil</a>
-    </div>
-  </div>
-
-
-  <script>
-    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-    function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
-      document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
-    }
-
-    /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-    function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-      document.body.style.backgroundColor = "white";
-    }
-    // // document.getElementById('img-1').addEventListener('click', function() {
-    // // document.getElementById('menu-lateral').style.left = '0'; 
-    // });
-  </script>
+      /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        document.body.style.backgroundColor = "white";
+      }
+      // // document.getElementById('img-1').addEventListener('click', function() {
+      // // document.getElementById('menu-lateral').style.left = '0'; 
+      // });
+    </script>
 </body>
