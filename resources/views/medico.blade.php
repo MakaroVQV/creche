52411174f9cd7f0a4b2cab1272ef7af9237b12ef
Luis Felipe <107706177+felipanos@users.noreply.github.com>
2023-05-04 19:27:49 -0300

    attmedico

--- resources/views/medico.blade.php
+++ resources/views/medico.blade.php
@@ -64,57 +64,78 @@
   <!--Ficha, Atestado e cardápio-->
 
   <div class="invisible">
-    <div class="cards">
-      <img src="{{asset('/imagens/icon ficha medica.png')}}">
-      <p>Ficha Médica</p>
-    </div>
+        <div class="cards" id="ficha-medica">
+          <img src="../imagens/icon ficha medica.png">
+          <p>Ficha Médica</p>
+          <div class="texto-expandido">
+            <h1>alou</h1>
+            <div class="detalhe">
+                <div class="info"><img src="../imagens/boneco.png"><p>Atura e peso</p></div><div class="data">fev de 2023</div></div>
+            <div class="detalhe">
+                <div class="info"><img src="../imagens/boneco.png"></div> <div class="data">fev de 2023</div></div>
+            <div class="detalhe">
+                <div class="info"><img src="../imagens/boneco.png"></div> <div class="data">fev de 2023</div></div>
+            <div class="detalhe">
+                <div class="info"><img src="../imagens/boneco.png"></div> <div class="data">fev de 2023</div></div>
+            <div class="detalhe">
+                <div class="info"><img src="../imagens/boneco.png"></div> <div class="data">fev de 2023</div></div>
+            <div class="detalhe">
+                <div class="info"><img src="../imagens/boneco.png"></div> <div class="data">fev de 2023</div></div>
+          </div>
+        </div>
 
     <div class="cards">
-      <img src="{{asset('/imagens/icon atestados.png')}}">
+      <img src="../imagens/icon atestados.png">
       <p>Atestado</p>
+      <div class="texto-expandido">
+        <p>Aqui está o texto expandido para a atestado.</p>
+      </div>
     </div>
 
     <div class="cards">
-      <img src="{{asset('/imagens/icon cardapio.png')}}">
+      <img src="../imagens/icon cardapio.png">
       <p>Cardápio</p>
+      <div class="texto-expandido">
+        <p>Aqui está o texto expandido para a Cardápio.</p>
+      </div>
     </div>
   </div>
 
+  <div class="footer"></div>
+
+      <script>
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+// constante para os cards cards se abrirem
+
+const cards = document.querySelectorAll('.cards');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    const textoExpandido = card.querySelector('.texto-expandido');
+    card.classList.toggle('expandido');
+    textoExpandido.classList.toggle('mostrar');
+    card.classList.toggle('card-normal'); // Adicione essa linha para alternar entre as classes
+  });
+});
 
 
-  <!--Menu lateral-->
-  <div id="mySidenav" class="sidenav">
-    <div class="side">
-      <a href="javascript:void(0)" onclick="closeNav()"><img src="{{asset('/imagens/icon menu.png')}}" class="menu-cinza"></a>
-      <div><img src="{{asset('/imagens/creche.png')}}" class="logo-sidebar"></div>
-    </div>
-    <a href="javascript:void(0)" onclick="closeNav()"><img src="{{ url('/imagens/seta-azul.png')}}" class="seta-azul"></a>
-    <div class="iten-menu">
-      <a href="{{ url('/notificado')}}"><img src="{{asset('/imagens/icon calendario cinza.png')}}">Calendario</a>
-      <a href="{{ url('/financa')}}"><img src="{{asset('/imagens/icon financeiro cinza.png')}}">Financeiro</a>
-      <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon home azul.png')}}">Home</a>
-      <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon saude cinza.png')}}">Saude</a>
-      <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon menu.png')}}">Perfil</a>
-    </div>
-  </div>
+        // // document.getElementById('img-1').addEventListener('click', function() {
+        // // document.getElementById('menu-lateral').style.left = '0'; 
+        // });
+      </script>
 
-
-  <script>
-    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-    function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
-      document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
-    }
-
-    /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-    function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-      document.body.style.backgroundColor = "white";
-    }
-    // // document.getElementById('img-1').addEventListener('click', function() {
-    // // document.getElementById('menu-lateral').style.left = '0'; 
-    // });
-  </script>
+  
 </body>
