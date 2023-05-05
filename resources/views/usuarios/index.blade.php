f0c8b7915c1435047fb2651cf88c31681b07bdcc
Erick D <eridanpersan@gmail.com>
2023-05-05 20:10:34 -0300

    att

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -2,14 +2,11 @@
 
 @section('content')
 
-    <h1>Visualizar Usuários</h1>
-    <a href="{{ url('usuarios/create')}}">CRIAR</a>
-    @if (session('status'))
-    <div class="alert alert-success">
-        {{session('status')}}
-    </div>
-    @endif
-    <table>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<h1>Visualizar Usuários</h1>
+
+<div class="container">
+    <table class="table table-dark">
         <thead>
             <tr>
                 <th>Nome</th>
@@ -19,17 +16,19 @@
         <tbody>
             @foreach($users as $user)
             <tr>
-                <td>{{ $user->name }}</td>
-                <td>
-                    <a class="btn btn-primary" href="{{ url('usuarios/'. $user->id) }}">Vizualizar </a>
-                    <a class="btn btn-warning" href="{{ url('usuarios/'. $user->id . '/edit') }}">Editar </a>
+                <td class="col-sm">{{ $user->name }}</td>
+                <td class="">
+                    <a class="col-sm btn btn-success" href="{{ url('usuarios/create')}}">Criar</a>
+                    <a class="col-sm btn btn-primary" href="{{ url('usuarios/'. $user->id) }}">Vizualizar</a>
+                    <a class="col-sm btn btn-warning" href="{{ url('usuarios/'. $user->id . '/edit') }}">Editar</a>
                     {!! Form::open(['method' => 'DELETE','route' =>['usuarios.destroy', $user->id],'style'=>'display:inline']) !!}
-                    {{Form::submit('Excluir', ['class' => 'btn btn-danger'])}}
+                    {{Form::submit('Excluir', ['class' => 'col-sm btn btn-danger'])}}
                     {!! Form::close() !!}
                 </td>
             </tr>
             @endforeach
         </tbody>
     </table>
+</div>
 
 @endsection
