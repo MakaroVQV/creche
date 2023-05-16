794cd115f6a274ff4e9674996d0b32fb50d3799d
MakaroVQV <marco_antonio1999@outlook.com.br>
2023-05-16 19:50:47 -0300

    att correção

--- resources/views/Financa.blade.php
+++ resources/views/Financa.blade.php
@@ -26,30 +26,34 @@
     </navbar>
 
     <!--Menu lateral-->
- <!-- Menu acima-->
- <div class="main">
-        <navbar class="navbar">
-            <div class="esquerda-nav">
-                <div onclick="openNav()"><img src="{{asset('/imagens/icon menu.png')}}" class="menu-azul"></div>
-                <div><img src="{{asset('/imagens/creche.png')}}" class="logo-navbar"></div>
-            </div>
-            <div class="direita-nav">
-                <div class="texto-user">
-                    <h5><strong>Aluno(a)</strong></h5>
-                </div>
-                <div><img src="{{asset('/imagens/icon aluna.png')}}" class="logo-user"></div>
-            </div>
-        </navbar>
-
-        <!--Menu lateral-->
-        <div id="mySidenav" class="sidenav">
-    <div class="side">
-      <a href="javascript:void(0)" onclick="closeNav()"><img src="{{asset('/imagens/icon menu.png')}}" class="menu-cinza"></a>
-      <div><img src="{{asset('/imagens/creche.png')}}" class="logo-sidebar"></div>
-    </div>
-    <div class="iten-menu">
-      <a href="{{ url('/calendario')}}"><img src="../imagens/icon calendario cinza.png">
-        <p>Calendario</p></a>
+    <div id="mySidenav" class="sidenav">
+      <div class="side">
+        <a href="javascript:void(0)" onclick="closeNav()"><img src="{{asset('/imagens/icon menu.png')}}" class="menu-cinza"></a>
+        <div><img src="{{asset('/imagens/creche.png')}}" class="logo-sidebar"></div>
+      </div>
+      <div class="iten-menu">
+        <a href="{{ url('/calendario')}}"><img src="{{asset('/imagens/icon calendario cinza.png')}}">
+          <p>Calendario</p>
+        </a>
+        <a href="{{ url('/financa')}}"><img src="{{asset('/imagens/icon financeiro cinza.png')}}">
+          <p>Financeiro</p>
+        </a>
+        <a href="{{ url('/home')}}"><img src="{{asset('/imagens/icon home azul.png')}}">
+          <p>Home</p>
+        </a>
+        <a href="{{ url('/medico')}}"><img src="{{asset('/imagens/icon saude cinza.png')}}">
+          <p>Saude</p>
+        </a>
+        <a href="{{ url('/perfil')}}"><img src="{{asset('/imagens/icon user cinza.png')}}">
+          <p>Perfil</p>
+        </a>
+      </div>
+      <!--Footer do menu lateral-->
+      <div class="footer-menu">
+        <a href="index.html"><img src="{{asset('/imagens/sair.png')}}" class="sair">
+          <p>Sair</p>
+        </a>
+      </div>
 
       <div class="copyriht">
         <img src="{{asset('/imagens/icon_copyright.png')}}" class="copyright">
