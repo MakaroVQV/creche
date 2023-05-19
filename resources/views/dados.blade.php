c3339a3d0a1d0b9d7361875289c2c3a745e4448a
Maria Carmo <mariabcarmo@gmail.com>
2023-05-19 20:22:56 -0300

    att

--- resources/views/dados.blade.php
+++ resources/views/dados.blade.php
@@ -12,17 +12,20 @@
         <div class="atualizar">
             <h3>Atualizar meus dados</h3>
                 <form>
-                    <input class="field" name="email" placeholder="E-mail">
-                    <input class="field" name="pais" placeholder="Selecionar país">
-                    <div class="form-group col-xs-2">
-                        <label for="ddd">* DDD</label>
-                        <input type="text" class="form-control" id="ddd" placeholder="00">
-                      </div>
-                      <div class="form-group col-xs-10">
-                        <label for="tel">* TELEFONE</label>
-                        <input type="text" class="form-control" id="tel" placeholder="00000-0000">
-                      </div>
-                    <input type="submit" value="Atualizar">
+                    <div>
+                        <label for="email">* E-mail</label>
+                        <input class="field" name="email" placeholder="E-mail">
+                    </div>
+                        <div>
+                        <div class="form-group col-xs-2">
+                            <label for="ddd">* DDD</label>
+                            <input type="text" class="form-control" id="ddd" placeholder="00">
+                        </div>
+                        <div class="form-group col-xs-10">
+                            <label for="tel">* Telefone</label>
+                            <input type="text" class="form-control" id="tel" placeholder="00000-0000">
+                        </div>
+                            <input type="submit" value="Atualizar">
                 </form>
         </div>
     </section>
---
+++ resources/views/feed.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form name="CadastroAlunos" type="text" method="post" enctype="multipart/form-data" action="upload.php">
+        <input type="text" name="Titulo" placeholder="Título">
+        <input type="text" name="texto" placeholder="Legenda">
+        <input type="file" name="image" placeholder="Imagem">
+            <input type="submit" value="Enviar"/>
+  </form>
+</body>
+</html>
