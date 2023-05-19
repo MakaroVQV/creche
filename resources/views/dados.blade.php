684f5c51772a27dcc770683dbcb74aa0f5c2bc50
Maria Carmo <mariabcarmo@gmail.com>
2023-05-19 20:00:06 -0300

    att

--- resources/views/dados.blade.php
+++ resources/views/dados.blade.php
@@ -13,8 +13,15 @@
             <h3>Atualizar meus dados</h3>
                 <form>
                     <input class="field" name="email" placeholder="E-mail">
-                    <input class="field" name="telefone" placeholder="Telefone">
                     <input class="field" name="pais" placeholder="Selecionar país">
+                    <div class="form-group col-xs-2">
+                        <label for="ddd">* DDD</label>
+                        <input type="text" class="form-control" id="ddd" placeholder="00">
+                      </div>
+                      <div class="form-group col-xs-10">
+                        <label for="tel">* TELEFONE</label>
+                        <input type="text" class="form-control" id="tel" placeholder="00000-0000">
+                      </div>
                     <input type="submit" value="Atualizar">
                 </form>
         </div>
