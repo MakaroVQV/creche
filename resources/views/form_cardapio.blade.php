fd29361c479612e22d03881c7322f21b2b48e066
Maria Carmo <mariabcarmo@gmail.com>
2023-05-22 20:57:03 -0300

    update

---
+++ resources/views/form_cardapio.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cardápio</title>
+</head>
+<body>
+    <section class="content">
+        <div class="atualizar">
+            <h3>Atualizar Cardápio</h3>
+                <form>
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input class="field" name="email" placeholder="E-mail">
+                    </div>
+                        <div>
+                        <div class="form-group col-xs-2">
+                            <label for="ddd">DDD</label>
+                            <input type="text" class="form-control" id="ddd" placeholder="00">
+                        </div>
+                        <div class="form-group col-xs-10">
+                            <label for="tel">* Telefone</label>
+                            <input type="text" class="form-control" id="tel" placeholder="00000-0000">
+                        </div>
+                            <input type="submit" value="Atualizar">
+                </form>
+        </div>
+    </section>
+</body>
+</html>
