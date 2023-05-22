8f7337d0dbe5bf3e7b9c14db4c73ffd90a1e117f
Leonardo Mello <82420373+orinmello@users.noreply.github.com>
2023-05-22 20:27:19 -0300

    tables

---
+++ database/migrations/2023_05_22_223627_create_ficha_medicas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ficha_medicas', function (Blueprint $table) {
+            $table->id();
+            $table->string('altura');
+            $table->string('peso');
+            $table->string('alergias');
+            $table->string('medicamentos');
+            $table->string('tipo sanguineo');
+            $table->string('vacinas');
+            $table->string('observacoes');
+            $table->string('aluno_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ficha_medicas');
+    }
+};
