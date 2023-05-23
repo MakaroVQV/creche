cfc823dc79100b2a711728a49edf721ce0bdc981
Chicão <92949475+Francisjr1210@users.noreply.github.com>
2023-05-22 21:35:27 -0300

    form_ficha_medica

---
+++ resources/views/form_ficha_medica.blade.php
@@ -0,0 +1,63 @@
+@extends('adminlte::page')
+
+@section('content')
+    <title>Visualizar Usuários</title>
+</head>
+<body>
+    
+    <style>
+        body{}
+   form {
+    position: absolute;
+width: 413px;
+height: 799px;
+left: 352px;
+top: 126px;
+
+background: #F5F5F5;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+    }
+
+ul{
+    height: 90px;
+    width: 342px;
+    left: 391px;
+    top: 288px;
+    border-radius: 10px;
+    padding: ;
+
+/* Rectangle */
+    width: 342px;
+    height: 90px;
+    left: 391px;
+    top: 288px;
+
+    background: #F6E5CA;
+    border-radius: 10px;
+
+
+}
+
+
+
+
+
+    </style>
+
+
+    <h1> Resumo </h1>
+
+    <form class="form-style-7">
+<ul>
+<li>
+    <label for="altura_e_peso">Altura e Peso </label>
+    <input type="number" name="altura_e_peso" maxlength="100">
+</li>
+</ul>
+
+
+
+
+</body>
+
