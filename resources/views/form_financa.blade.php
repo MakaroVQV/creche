60c91b351b0c8f0c86b26244b38dea0a52645d52
Maria Carmo <mariabcarmo@gmail.com>
2023-05-25 21:01:04 -0300

    att

--- resources/views/form_financa.blade.php
+++ resources/views/form_financa.blade.php
@@ -1,38 +1,32 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>Atualização de Mensalidade</h1>
-
-
-<section class="content">
-        <div class="atualizar">
-            
-                <form>
-                    <div>
-                        <label for="user">Vencimento</label>
-                        <input class="field" name="user" placeholder="R$" required>
-                    </div>
-                    <div>
-                        <label for="name">Valor</label>
-                        <input class="field" name="name" placeholder="R$" required>
-                    </div>
-                    <div>
-                        <label for="email">Valor Pago</label>
-                        <input class="field" name="email" placeholder="R$" required>
-                    </div>
-                        <div>
-                            
+    <h1>Atualização de Mensalidade</h1>
+        <section class="content">
+                <div class="atualizar">
+                    
+                        <form>
                             <div>
-                            <form action="">
-                                <label for="Status">Status</label>
-                                 <select name=""  id="">
-                                
-                                    <option value="Pendente">Pendente</option>
-                                        <option value="Paga">Paga</option>
-                                        
+                                <label for="user">Vencimento</label>
+                                <input class="field" name="user" placeholder="R$" required>
+                            </div>
 
-</div>
+                            <div>
+                                <label for="name">Valor</label>
+                                <input class="field" name="name" placeholder="R$" required>
+                            </div>
+
+                            <div>
+                                <label for="email">Valor Pago</label>
+                                <input class="field" name="email" placeholder="R$" required>
+                            </div>
+                                    
+                            <div>
+                                <label for="Status">Status</label>
+                                <select name=""  id="">
+                                <option value="Pendente">Pendente</option>
+                                <option value="Paga">Paga</option>
+                            </div>
 </form>
 <input type="submit" value="Atualizar">
                
