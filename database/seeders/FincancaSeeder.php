73428e73c39a0f623aeed6c646acf9e721768173
Leonardo Mello <82420373+orinmello@users.noreply.github.com>
2023-05-29 19:39:31 -0300

    seed fincacas

--- database/seeders/FincancaSeeder.php
+++ database/seeders/FincancaSeeder.php
@@ -4,14 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class FincancaSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        DB::table('financas')->insert([
+            'vencimento'          => '2023-05-01',
+            'valor'               => 700.00,
+            'valor_pagos'         => 700.00,
+            'status'              => 'pago'
+        ]);
+    
+        DB::table('financas')->insert([
+            'vencimento'          => '2023-04-01',
+            'valor'               => 700.00,
+            'valor_pagos'         => 700.00,
+            'status'              => 'vencido'
+        ]);
+    
+        DB::table('financas')->insert([
+            'vencimento'          => '2023-06-01',
+            'valor'               => 700.00,
+            'valor_pagos'         => 700.00,
+            'status'              => 'aberto'
+        ]);
     }
 }
