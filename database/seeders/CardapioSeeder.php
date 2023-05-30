1d7924cacc2eaf83ecef7c1e55964d3195980934
Leonardo Mello <82420373+orinmello@users.noreply.github.com>
2023-05-29 21:13:41 -0300

    upt cardapio

--- database/seeders/CardapioSeeder.php
+++ database/seeders/CardapioSeeder.php
@@ -4,19 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CardapioSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
-    {
-        
-    }
+        {
+            DB::table('cardapio')->insert([
+                'colocao'          => 'Mini pão francês com manteiga;
+                Café com leite',
+                'lache_manha'      => 'Biscoito maionese;
+                Suco de maracujá',
+                'almoco'           => 'Arroz, Feijão preto, Carne moída, Macarrão a bolonhesa;
+                Suco: Limonada;
+                Sobremesa: Maçã',
+                'lache_tarde'      => 'Duo de frutas (maçã e banana)'
+            ]);
+        }
 }
 
-$table->string('colocao');
-$table->string('lache_manha');
-$table->string('almoco');
-$table->string('lache_tarde');
