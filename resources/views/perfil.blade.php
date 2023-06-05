9b329b390d9538fbd57d6000802a341309ec228f
Maria Carmo <mariabcarmo@gmail.com>
2023-06-05 20:20:37 -0300

    at

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -40,7 +40,6 @@
         <div class="retangulo">
       <a href="#" onclick="toggleSubMenu('subMenuFinanca')">
         <img src="{{asset('/imagens/icon financeiro cinza.png')}}">
-        <div class="esquerda"><img src="{{asset('/imagens/icon seta cinza.png')}}" id="seta1"></div>
         <p>Financeiro</p>
         </div>
         <div id="subMenuFinanca" class="sub-menu">
@@ -59,7 +58,6 @@
       <div class="retangulo">
       <a href="#" onclick="toggleSubMenu('subMenuMedico')">
         <img src="{{asset('/imagens/icon saude cinza.png')}}">
-        <div class="esquerda"><img src="{{asset('imagens/icon seta cinza.png')}}" id="seta2"></div>
         <p>Saude</p>
         </div>
 
@@ -85,7 +83,7 @@
     </div>
   
     <div class="copyriht"> 
-      <img src="{{asset('/imagens/icon copyright preto.png')}}" class="copyright">
+      <img src="{{asset('/imagens/icon_copyright.png')}}" class="copyright">
       <p><strong>2023 FROM NEWPATH</strong></p>
     </div>
   </div>
