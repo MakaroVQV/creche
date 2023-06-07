b5f52e8d0ee9c085751d401677c4c542f292e284
Maria Carmo <mariabcarmo@gmail.com>
2023-06-07 20:53:47 -0300

    update

---
+++ resources/views/politicas.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('/css/politicas.css')}}">
+    <title>Termo de uso e Política de Provacidade</title>
+</head>
+<body>
+    <h1>Política de Privacidade</h1>
+    <div class="politicas-container">
+        <p>Na <strong>Creche Escola Jardim dos Sonhos</strong>, privacidade e segurança são prioridades e nos comprometemos com a transparência do tratamento de dados pessoais dos nossos usuários/clientes.
+            Por isso, está presente Política de Privacidade estabelece como é feita a coleta, uso e transferência de informações de clientes ou outras pessoas que acessam ou usam nosso site.
+            Ao utilizar nossos serviços, você entende que coletamos e usamos suas informações pessoais nas formas descritas nesta Política, sob as normas da Constituição Federal de 1988 (art. 5º, LXXIX; e o art. 22º, XXX – incluídos pela EC 115/2022), das normas de Proteção de Dados (LGPD, Lei Federal 13.709/2018), das disposições consumeristas da Lei Federal 8078/1990 e as demais normas do ordenamento jurídico brasileiro aplicáveis.
+            Dessa forma, a <strong>Sociedade Educacional Amaral Lima Ltda</strong>, doravante denominada simplesmente como <strong>“Creche Escola Jardim dos Sonhos”</strong>, inscrita no CNPJ/MF sob o nº <strong>20.058.776/0001-70</strong>, no papel de Controladora de Dados, obriga-se ao disposto na presente Política de Privacidade.
+        </p>
+
+        <p>Nosso site coleta e utiliza alguns dados pessoais seus, de forma a viabilizar a prestação de serviços e aprimorar a experiência de uso.</p>
+           
+        <p>É a partir do seu <strong>consentimento</strong> que tratamos os seus dados pessoais. O consentimento é a manifestação livre, informada e inequívoca pela qual você autoriza a <strong>
+            Creche Escola Jardim dos Sonhos</strong> a tratar seus dados. Assim, em consonância com a Lei Geral de Proteção de Dados, seus dados só serão 
+            coletados, tratados e armazenados mediante prévio e expresso consentimento.</p>
+            
+            
+        <p>O seu consentimento será obtido de forma específica para cada finalidade acima descrita, evidenciando o compromisso de transparência e boa-fé 
+            da <strong>Creche Escola Jardim dos Sonhos</strong> para com seus usuários/clientes, seguindo as regulações legislativas pertinentes.</p>
+            
+            
+        <p>Ao utilizar os serviços da <strong>Creche Escola Jardim dos Sonhos</strong> e fornecer seus dados pessoais, você está ciente e consentindo 
+            com as disposições desta Política de Privacidade, além de conhecer seus direitos e como exercê-los. A qualquer tempo e sem nenhum custo, 
+            você poderá revogar seu consentimento.</p>
+            
+            
+        <p>É importante destacar que a revogação do consentimento para o tratamento dos dados pode implicar a impossibilidade da performance adequada 
+            de alguma funcionalidade do site que dependa da operação. Tais consequências serão informadas previamente.</p>
+
+        <p>A <strong>Creche Escola Jardim dos Sonhos</strong> assegura a seus usuários/clientes seus <strong>direitos de titular</strong> 
+            previstos no artigo 18 da Lei Geral de Proteção de Dados. Dessa forma, você pode, de maneira gratuita e a qualquer tempo:</p>
+        <ul>
+            <li><strong>Confirmar a existência de tratamento de dados</strong>, de maneira simplificada ou em formato claro e completo.</li>
+            <li><strong>Acessar seus dados</strong>, podendo solicitá-los em uma cópia legível sob forma impressa ou por meio eletrônico, seguro e idôneo.</li>
+            <li><strong>Corrigir seus dados</strong>, ao solicitar a edição, correção ou atualização destes.</li>
+            <li><strong>Limitar seus dados</strong> quando desnecessários, excessivos ou tratados em desconformidade com a legislação através da anonimização, bloqueio ou eliminação.</li>
+            <li><strong>Solicitar a portabilidade de seus dados</strong>, através de um relatório de dados cadastrais que a <strong>Creche Escola Jardim dos Sonhos</strong> trata a seu respeito.</li>
+            <li><strong>Eliminar seus dados</strong> tratados a partir de seu consentimento, exceto nos casos previstos em lei.</li>
+            <li><strong>Revogar seu consentimento</strong>, desautorizando o tratamento de seus dados.</li>
+            <li><strong>Informar-se sobre a possibilidade de não fornecer seu consentimento</strong> e sobre as consequências da negativa.</li>
+        </ul>
+
+        <p>Para exercer seus direitos de titular, você deve entrar em contato com a <strong>Creche Escola Jardim dos Sonhos</strong> através do seguinte 
+            meio disponivel:</p>
+            <li><a href="mailto:crechescolajardimdosonhos@gmail.com?subject=" link HTML>crechescolajardimdosonhos@gmail.com</a></li>
+
+            <p>De forma a garantir a sua correta identificação como titular dos dados pessoais objeto da solicitação, é possível que solicitemos documentos ou 
+                demais comprovações que possam comprovar sua identidade. Nessa hipótese, você será informado previamente.</p>
+    </div>
+
+</body>
+</html>
