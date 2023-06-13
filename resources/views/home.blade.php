39e676c61c10f97fde91a159ff8b7dd757acd5f6
Luis Felipe <107706177+felipanos@users.noreply.github.com>
2023-06-13 19:08:08 -0300

    att3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -127,12 +127,10 @@
             <div class="imagem-centro">
 
                 <img src="{{asset('/imagens/poster_home.jpg')}}">
-                </div>
-            <center>
+                </div>        
             <div class="descricao">
                 <p>Alunos e Ex-alunos, convidamos você e sua família para comemorar os nossos 10 anos junto de um incrível arrai, teremos diversas comidas típicas, além de incríveis gincanas e muitas brincadeiras.</p>
                 <p>Não perca tempo, venha se divertir conosco.</p>
-            </center>
             </div>
         </div>
     </div>
